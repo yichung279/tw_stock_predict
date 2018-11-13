64cf8f9a165e207c814800604c90e426499905b8
yichung <ql3895619@gmail.com>
2018-11-13 15:24:50 +0800

    Add diff stock in preprocess

--- preprocess.py
+++ preprocess.py
@@ -7,8 +7,12 @@ from pprint import pprint
 jsonlist = glob("stock_history/*")
 jsonlist.sort()
 
-stock_codes = ['3481', '2408', '2454', '3231', '3008', '2330', '2451', '2388', '2379', '2890']
-normal =      [10,     100,    480,     30,    5000,   250,    110,    50,     130,    13]
+# stock_codes = ['3481', '2408', '2454', '3231', '3008', '2330', '2451', '2388', '2379', '2356'] # tech
+# normal =      [10,     100,    480,     30,    5000,   250,    110,    50,     130,    25]
+# stock_codes = ['2801','2809', '2812', '2836', '2845', '2887', '2886', '2885', '2884', '2890'] # bank
+# normal = [18, 40, 10, 10, 10, 15, 25, 15, 20, 13]
+stock_codes = ['2002', '1402' ,'1314', '2023', '1102', '2014', '1301', '1326', '1312', '1710'] # tradition
+normal = [25, 35, 15, 13, 45, 15, 110, 125, 30, 32]
 stocks = np.zeros((10, 1000, 5))
 
 for j, jsonname in enumerate(jsonlist[-1000:]) :
@@ -30,4 +34,4 @@ for i in range(9810):
             day = (i % 981) + j
             data[i][j][k] = stocks[st_code][day][k] / normal[st_code]
 
-np.save('stock_data.npy', data)
+np.save('training_data/tradition_data.npy', data)
